b4675c5771557c921bb769f919b410e5f9da9eb3
Daniel Batista Da Silva <dbatista@c2r4s1.42.rio>
2025-03-27 15:38:15 -0300

    alterando para ler sem o nome do programa

--- teste1/teste.c
+++ teste1/teste.c
@@ -6,7 +6,7 @@
 /*   By: dbatista <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:58:48 by dbatista          #+#    #+#             */
-/*   Updated: 2025/03/27 15:08:05 by dbatista         ###   ########.fr       */
+/*   Updated: 2025/03/27 15:36:10 by dbatista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,7 +15,7 @@ int	main(int argc, char **argv)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	while (i < argc)
 	{
 		printf("%i - %s\n", i, argv[i]);
